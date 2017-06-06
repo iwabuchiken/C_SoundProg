7c2bb43e3e6a142d790cf37e7dcedfb269f632a6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-06 17:42:57 +0900

    Prog / D-4 10#1 / C_SoundProg / 20170606_130618
    steps=~1.10
    ---
    #) TINK-6 : sine squared ---> use new functions
    #) TINK-7 : sine cube ---> use new functions
    
    	==> done
    
    ---------------- TEMPLATE
    1.  --> .
    
    	==> done

--- main/D-6/ex1_1.c
+++ main/D-6/ex1_1.c
@@ -21,11 +21,14 @@ int write_PCMData_to_File(void);
 //int write_PCMData_to_File(MONO_PCM pcm);
 void absolutize_Sound_Data();
 void sine_Squared();
+void sine_Squared__V2();
 void sine_P4();
 void volume_Down_By_Percent(int nominator);
 int volume_Down_PCMData_By_Percent(MONO_PCM* pcm0, int nominator);
 void absolutize_MONO_PCM(MONO_PCM* pcm);
 int absolutize_MONO_PCM__2PCMs(MONO_PCM* pcm1, MONO_PCM* pcm2);
+
+void show_Basic_PCM_Data(MONO_PCM* pcm0);
 
 /********************************************************
  *
@@ -581,6 +584,90 @@ void sine_Cube() {
 
 }//void sine_Cube()
 
+void sine_Cube__V2() {
+
+	MONO_PCM pcm0, pcm1;
+	int n;
+
+	//  mono_wave_read(&pcm0, "a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+	mono_wave_read(&pcm0, "C:\\WORKS_2\\WS\\Eclipse_Luna\\C_SoundProg\\main\\D-6\\a.wav");	//=> working
+
+	pcm1.fs = pcm0.fs; /* 標本化周波数 */
+	pcm1.bits = pcm0.bits; /* 量子化精度 */
+	pcm1.length = pcm0.length; /* 音データの長さ */
+	pcm1.s = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
+
+	/**********************
+		volume down
+	**********************/
+	volume_Down_PCMData_By_Percent(&pcm0, 999);
+
+	/**********************
+		sine cube
+	**********************/
+	for (n = 0; n < pcm1.length; n++)
+	{
+
+		//ref fabs https://stackoverflow.com/questions/20956352/how-to-get-absolute-value-from-double-c-language "answered Jan 6 '14 at 18:08"
+		pcm1.s[n] = pcm0.s[n] * pcm0.s[n] * pcm0.s[n];	// sin^2
+//		pcm1.s[n] = pcm0.s[n] * pcm0.s[n];	// sin^2
+//		pcm1.s[n] = fabs(pcm0.s[n]); /* 音データのコピー */
+//	  pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
+
+	}
+
+	char fname_dst[100];
+
+	char fname_trunk_full[50];
+
+	//  char* fname_trunk = "b";
+	char* fname_trunk = "tink-4_cube_V2";	// 14 chars	//=> file WAS generated (in this directory)
+//	char* fname_trunk = "main\\D-6\\tink-2";	//=> file WAS generated (in this directory)
+	//  char* fname_trunk = "\\main\\D-6\\b";	//=> file not generated (in this directory)
+	//  char* fname_trunk = "..\\main\\D-6\\b";	//=> file not generated (in this directory)
+
+	char* dirpath = "main\\D-6\\sound";	// 14 chars
+
+	sprintf(fname_trunk_full, "%s\\%s", dirpath, fname_trunk);
+
+	char* fname_ext = "wav";
+
+	 get_file_name_with_time_label(fname_dst, fname_trunk_full, fname_ext);
+//	 get_file_name_with_time_label(fname_dst, fname_trunk, fname_ext);
+
+	 printf("[%s:%d] fname_dst => '%s'\n", basename(__FILE__, '\\'), __LINE__, fname_dst);
+
+	 /**********************
+
+		get: dirname
+
+		**********************/
+	 char* dpath = dirname(__FILE__, '\\');
+
+	 printf("[%s:%d] dpath => '%s'\n", basename(__FILE__, '\\'), __LINE__, dpath);
+
+
+
+	mono_wave_write(&pcm1, fname_dst); /* WAVEファイルにモノラルの音データを出力する */
+	//  mono_wave_write(&pcm1, "b.wav"); /* WAVEファイルにモノラルの音データを出力する */
+	//=> with "b.wav" ---> the file gets created at "C:\WORKS_2\WS\Eclipse_Luna\C_SoundProg"
+
+	/**********************
+
+		TINK-2
+
+	**********************/
+
+	/********************************************************
+	 *
+	 * free
+	 *
+	*********************************************************/
+	free(pcm0.s); /* メモリの解放 */
+	free(pcm1.s); /* メモリの解放 */
+
+}//void sine_Cube__V2()
+
 void sine_Squared() {
 
 	MONO_PCM pcm0, pcm1;
@@ -655,6 +742,87 @@ void sine_Squared() {
 
 }//void sine_Squared()
 
+void sine_Squared__V2() {
+
+	MONO_PCM pcm0, pcm1;
+	int n;
+
+	//  mono_wave_read(&pcm0, "a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+	mono_wave_read(&pcm0, "C:\\WORKS_2\\WS\\Eclipse_Luna\\C_SoundProg\\main\\D-6\\a.wav");	//=> working
+
+	pcm1.fs = pcm0.fs; /* 標本化周波数 */
+	pcm1.bits = pcm0.bits; /* 量子化精度 */
+	pcm1.length = pcm0.length; /* 音データの長さ */
+	pcm1.s = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
+
+	/**********************
+		volume --> down
+	**********************/
+	volume_Down_PCMData_By_Percent(&pcm0, 999);
+
+
+	for (n = 0; n < pcm1.length; n++)
+	{
+
+		//ref fabs https://stackoverflow.com/questions/20956352/how-to-get-absolute-value-from-double-c-language "answered Jan 6 '14 at 18:08"
+		pcm1.s[n] = pcm0.s[n] * pcm0.s[n];	// sin^2
+//		pcm1.s[n] = fabs(pcm0.s[n]); /* 音データのコピー */
+//	  pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
+
+	}
+
+	char fname_dst[100];
+
+	char fname_trunk_full[50];
+
+	//  char* fname_trunk = "b";
+	char* fname_trunk = "tink-3_squared-V2";	// 14 chars	//=> file WAS generated (in this directory)
+//	char* fname_trunk = "main\\D-6\\tink-2";	//=> file WAS generated (in this directory)
+	//  char* fname_trunk = "\\main\\D-6\\b";	//=> file not generated (in this directory)
+	//  char* fname_trunk = "..\\main\\D-6\\b";	//=> file not generated (in this directory)
+
+	char* dirpath = "main\\D-6\\sound";	// 14 chars
+
+	sprintf(fname_trunk_full, "%s\\%s", dirpath, fname_trunk);
+
+	char* fname_ext = "wav";
+
+	 get_file_name_with_time_label(fname_dst, fname_trunk_full, fname_ext);
+//	 get_file_name_with_time_label(fname_dst, fname_trunk, fname_ext);
+
+	 printf("[%s:%d] fname_dst => '%s'\n", basename(__FILE__, '\\'), __LINE__, fname_dst);
+
+	 /**********************
+
+		get: dirname
+
+		**********************/
+	 char* dpath = dirname(__FILE__, '\\');
+
+	 printf("[%s:%d] dpath => '%s'\n", basename(__FILE__, '\\'), __LINE__, dpath);
+
+
+
+	mono_wave_write(&pcm1, fname_dst); /* WAVEファイルにモノラルの音データを出力する */
+	//  mono_wave_write(&pcm1, "b.wav"); /* WAVEファイルにモノラルの音データを出力する */
+	//=> with "b.wav" ---> the file gets created at "C:\WORKS_2\WS\Eclipse_Luna\C_SoundProg"
+
+	/**********************
+
+		TINK-2
+
+	**********************/
+
+	/********************************************************
+	 *
+	 * free
+	 *
+	*********************************************************/
+	free(pcm0.s); /* メモリの解放 */
+	free(pcm1.s); /* メモリの解放 */
+
+}//void sine_Squared()
+
 void absolutize_Sound_Data() {
 
 	MONO_PCM pcm0, pcm1;
@@ -668,11 +836,25 @@ void absolutize_Sound_Data() {
 	pcm1.length = pcm0.length; /* 音データの長さ */
 	pcm1.s = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
 
+	/**********************
+		Volume down
+	**********************/
+	volume_Down_PCMData_By_Percent(&pcm0, 999);
+
+
+	/**********************
+		absolutize
+	**********************/
+	absolutize_MONO_PCM(&pcm0);
+
+
+
 	for (n = 0; n < pcm1.length; n++)
 	{
 
 		//ref fabs https://stackoverflow.com/questions/20956352/how-to-get-absolute-value-from-double-c-language "answered Jan 6 '14 at 18:08"
-		pcm1.s[n] = fabs(pcm0.s[n]); /* 音データのコピー */
+		pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
+//		pcm1.s[n] = fabs(pcm0.s[n]); /* 音データのコピー */
 //	  pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
 
 	}
@@ -729,14 +911,30 @@ void absolutize_Sound_Data() {
 
 }//void absolutize_Sound_Data()
 
+void show_Basic_PCM_Data(MONO_PCM* pcm0) {
+
+//	int fs; /* 標本化周波数 */
+//	int bits; /* 量子化精度 */
+//	int length; /* 音データの長さ */
+//	double *s; /* 音データ */
+
+	printf("[%s:%d] Basic data\n", basename(__FILE__, '\\'), __LINE__);
+
+	printf("fs\t= %d\nbits\t= %d\nlength\t= %d", pcm0->fs, pcm0->bits, pcm0->length);
+
+
+}//void show_Basic_PCM_Data(MONO_PCM* pcm0)
+
 int main(void)
 {
 
-	volume_Down_By_Percent(999);
+//	volume_Down_By_Percent(999);
 //	volume_Down_By_Percent(99);
 //	sine_P4();
 //	sine_Cube();
+	sine_Cube__V2();
 //	sine_Squared();
+//	sine_Squared__V2();
 //	absolutize_Sound_Data();
 //	write_PCMData_to_File();
 //	test_ex1_x();
