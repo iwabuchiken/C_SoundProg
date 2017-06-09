8eabcc146a205fd0ef85b93bec5705f14e2313b9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-09 15:13:57 +0900

    Prog / D-4 13#1 / C_SoundProg / use python / 20170609_102131
    steps=~3.3
    ---
    ==> see mail
    
    	==> done
    
    ---------------- TEMPLATE
    1.  --> .
    
    	==> done

---
+++ python/13_1/audio/test_1.EqualTempera.20170609_121533.base-262.cue
@@ -0,0 +1,34 @@
+REM Created with Helium Audio Joiner - www.imploded.com
+FILE "test_1.EqualTempera.20170609_121533.base-262.wav" WAV
+  TRACK 01 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:01:00
+  TRACK 03 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:02:00
+  TRACK 04 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:03:00
+  TRACK 05 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:04:00
+  TRACK 06 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:05:00
+  TRACK 07 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:06:00
+  TRACK 08 AUDIO
+    PERFORMER ""
+    TITLE ""
+    INDEX 01 00:07:00
--- python/13_1/test_1.py
+++ python/13_1/test_1.py
@@ -18,6 +18,8 @@ from libs.libs import *		#=> C:\WORKS_2\WS\Eclipse_Luna\C_SoundProg\python\libs
 # from libs import *		#=> in 'libs' subdirectory --> see : https://stackoverflow.com/questions/1260792/import-a-file-from-a-subdirectory 'community wiki'
 						#=> libs.py : C:\WORKS_2\WS\WS_Others\free\K6H7DD_schroedinger\28_1\libs.py
 
+import libs.labs as labs
+
 import libs.wablibs as wl
 # import wablibs
 # import wablibs as wl
@@ -33,6 +35,22 @@ import wave
 import struct
 import numpy as np
 from pylab import *
+
+import random as rnd
+
+values = [1.000000,
+			1.059463,
+			1.122462,
+			1.189207,
+			1.259921,
+			1.334840,
+			1.414214,
+			1.498307,
+			1.587401,
+			1.681793,
+			1.781797,
+			1.887749,
+			2.000000]
 
 def show_Message() :
 				
@@ -89,6 +107,59 @@ def exec_1_Equal_Temperament():
 		
 	return
 				
+def exec_2_Random_Pitches():
+
+	'''
+		Gen sounds
+	'''
+	A = 1.0; fs = 8000.0; phase = 1.0; length = 1.0; dpath = 'audio'
+# 	A = 1.0; fs = 8000.0; phase = 1.0; length = 2.0; dpath = 'audio'
+	
+	timelabel = get_TimeLabel_Now()
+	
+	base_tone = 262		# 'A' pitch
+
+	#ref seed https://docs.python.jp/3/library/random.html
+	rnd.seed()
+	#ref https://stackoverflow.com/questions/4172131/create-random-list-of-integers-in-python 'answered Nov 13 '10 at 11:14'
+	#ref http://www.python-izm.com/contents/application/random.shtml
+	indexes = [rnd.randint(0, len(values) - 1) for i in range(0, 8)]
+# 	indexes = [rnd.randint(0, len(values)) for i in range(0, 8)]
+	
+	count = 1
+	
+	for num in indexes :
+# 	for val in values :
+		
+		base_tone = 262
+# 		base_tone = values[num]
+
+		current_tone = base_tone * values[num]
+		
+		#ref multiple string https://stackoverflow.com/questions/10660435/pythonic-way-to-create-a-long-multi-line-string 'answered May 18 '12 at 22:22'
+		fname = "test_1.EqualTempera.%s.(%d).base-%d_val-%.6f"\
+			"_tone-%.3f_A-%.3f_phase-%.3f_len-%.3f.wav" \
+				% (timelabel, count, base_tone, values[num], \
+				(base_tone * values[num]), A, phase, length)
+# 		fname = "test_1.EqualTempera.val-%.6f_tone-%.3f_A-%.3f_phase-%.3f_len-%.3f.%s.wav" \
+# 				% (val, (base_tone * val), A, phase, length, timelabel)
+		
+		wf = wl.get_SineWF(A, current_tone, fs, phase, length, dpath, fname)
+# 		wf = wl.get_SineWF(A, base_tone * val, fs, phase, length, dpath, fname)
+		
+		dpath_dst = "audio"
+	
+		print "[%s:%d] wf.fname => '%s'" % (thisfile(), linenum(), wf.fname)
+	
+		wl.save_WaveFile__Class(wf, dpath_dst=dpath_dst)
+		
+		# increment count
+		count += 1
+	
+	return
+	
+	
+	
 if __name__ == "__main__" :
 
 	'''###################
@@ -111,7 +182,8 @@ if __name__ == "__main__" :
 	'''###################
 		evecute		
 	###################'''
-	exec_1_Equal_Temperament()
+	exec_2_Random_Pitches()
+# 	exec_1_Equal_Temperament()
  	
  	
 	print
