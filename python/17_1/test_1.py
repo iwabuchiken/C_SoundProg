c68227a4a47409b9ed75af5af8b901bc1daea68d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-13 11:39:07 +0900

    Prog / D-4 17#1 / C_SoundProg / use python / 20170613_091159
    steps=2
    ---
    #) sin(x) + sin(x*n) ---> n is changable
    
    	==> done
    
    ---------------- TEMPLATE
    1.  --> .
    
    	==> done

--- python/17_1/test_1.py
+++ python/17_1/test_1.py
@@ -324,6 +324,152 @@ def exec_2__SinePlusSine2X_Standardize():
     ###################'''
     return
 
+def exec_3__SinePlusSineNX_Standardize():
+    
+    '''###################
+    	options		
+    ###################'''
+    options = get_opt_2(sys.argv, "fN")
+#     options = get_opt_2(sys.argv, "f")
+    
+    if not "-f" in [x[0] for x in options] :
+    	
+    	print "'-f' option needed"
+    	
+    	return
+    
+    if not "-N" in [x[0] for x in options] :
+    	
+    	print "'-N' option needed"
+    	
+    	return
+    
+#     print options
+#      
+#     return
+    
+    '''###################
+    	prep : wfs		
+    ###################'''
+    '''###################
+    	prep : wfs : Sines
+    ###################'''
+    A = 1.0; fs = 16000.0; length = 1.0; phase = 1.0; type = "sine"
+#     A = 1.0; fs = 8000.0; length = 1.0; phase = 1.0; type = "sine"
+    
+    f0_val = [x[1] for x in options if x[0] == '-f'][0]
+    
+    f0 = int(f0_val);
+#     f0 = 262 * wl.EQUAL_TEMPERAMENTS[6];
+#     f0 = 262 * wl.EQUAL_TEMPERAMENTS[3];
+#     f0 = 262;
+    
+    timelabel = get_TimeLabel_Now()
+    
+    fname_Sines = "test_1.sines.%s.f0=%d_fs=%.1f_phase-%1.2f.wav" % (timelabel, f0, fs, phase)
+    
+    wf_Sines = wl.get_WaveFile__Sines (fname_Sines, A, f0, fs, length, phase, type)
+
+#     #debug
+#     for i in range(0, 20) :
+#          
+#         print "[%s:%d] wf_Sines.radians[%d] => %.4f (of 2 * pi = %.4f)"\
+#                 % (thisfile(), linenum(), \
+#                    i, wf_Sines.radians[i], wf_Sines.radians[i] / np.pi)
+    
+
+    '''###################
+    	prep : wfs : Sines NX
+    ###################'''
+    # set N value
+    N = int([x[1] for x in options if x[0] == '-N'][0])
+#     N = [x[1] for x in options if x[0] == '-N'][0]
+    
+    # default value
+    if N == None : N = 3
+    
+    type = "sine"
+    
+    fname_SinesNX = "test_1.Sines%dX.%s.f0=%d_fs=%.1f_phase-%1.2f.wav" \
+                    % (N, timelabel, f0, fs, phase)
+    
+    wf_SinesNX_tmp = wl.get_WaveFile__Sines (\
+                        fname_SinesNX, A, f0, fs, length, phase, type)
+    
+    radian_values = [N * x for x in wf_SinesNX_tmp.radians]
+#     radian_values = [2 * x for x in wf_SinesNX_tmp.radians]
+#     radian_values = wf_Sines2X_tmp.radians
+    
+    wf_SinesNX_tmp.radians = radian_values
+    
+    wf_SinesNX = wl.update_WaveFile(wf_SinesNX_tmp, "radians")  #=> works
+    
+
+    '''###################
+        prep : wfs : Sines + Sines nX
+    ###################'''
+    analogdata_SinesPlusSinesNX = [sum(x) for x \
+                               in zip(wf_Sines.analogdata, wf_SinesNX.analogdata)]
+    
+    ### memo
+    # if not standardized, then struct.pack with "h" param in wablibs.py ==> Gets error
+    analogdata_SinesPlusSinesNX = wl.standardize_Data(analogdata_SinesPlusSinesNX)
+    
+    fname_SinesPlusSinesNX = \
+                "test_1.SinesPlusSines%dX.%s.f0=%d_fs=%.1f_phase-%1.2f.wav" \
+                % (N, timelabel, f0, fs, phase)
+    
+    wf_SinesPlusSinesNX = wl.get_WaveFile__AnalogData(\
+              fname_SinesPlusSinesNX, \
+              analogdata_SinesPlusSinesNX, \
+              A, length, \
+              nchannels = 1, radians = None, \
+              f0 = None, fs = None)
+
+    '''###################
+        Freq : Sines        
+    ###################'''
+    ### Sines
+    result_Sines = wl.measure_Frequency_4(wf_Sines)
+    
+    print "[%s:%d] result_Sines =>" % (thisfile(), linenum())
+    
+    print result_Sines
+
+    ### SinesNX
+    result_SinesNX = wl.measure_Frequency_4(wf_SinesNX)
+    
+    print "[%s:%d] result_SinesNX =>" % (thisfile(), linenum())
+    
+    print result_SinesNX
+
+    '''###################
+        save : wave file        
+    ###################'''
+    ### Sines
+    dpath = "audio"
+    
+    wl.save_WaveFile__Class(wavefile = wf_Sines, fname_dst = wf_Sines.fname, dpath_dst=dpath)
+
+    ### Sines 2X
+    wf = wf_SinesNX
+    wl.save_WaveFile__Class(wavefile = wf, fname_dst = wf.fname, dpath_dst=dpath)
+
+    ### Sines + Sines 2X
+    wf = wf_SinesPlusSinesNX
+    wl.save_WaveFile__Class(wavefile = wf, fname_dst = wf.fname, dpath_dst=dpath)
+
+    '''###################
+    	ending		
+    ###################'''
+    print "[%s:%d] exec_2__SinePlusSineNX_Standardize ==> done" % (thisfile(), linenum())
+    		
+    
+    '''###################
+    	return		
+    ###################'''
+    return
+
 if __name__ == "__main__" :
 
     '''###################
@@ -346,7 +492,8 @@ if __name__ == "__main__" :
     '''###################
     	evecute		
     ###################'''
-    exec_2__SinePlusSine2X_Standardize()
+    exec_3__SinePlusSineNX_Standardize()
+#     exec_2__SinePlusSine2X_Standardize()
 #     exec_1__SinePlusCosine_Standardize()
     
     print
